c2ef0643d13358ed4567a63a0765a53f004a2db2
Tristan Villanueva <tan_vil@hotmail.com>
2015-12-03 17:12:01 -0500

    finished panel 6

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "comicbook/comic3"
   get "comicbook/comic4"
   get "comicbook/comic5"
+  get "comicbook/comic6"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
